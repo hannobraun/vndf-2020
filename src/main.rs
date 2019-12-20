b7c90ecd2f18e6da707356bbe783ba58d42451ed
Hanno Braun <hb@hannobraun.de>
2019-12-20 22:19:33 +0100

    Replace "Hello, world!" with simple window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use ggez::{
+    Context,
+    ContextBuilder,
+    GameResult,
+    event::{
+        EventHandler,
+        run,
+    },
+};
+
+
+fn main() -> GameResult {
+    // Force X11 backend to prevent panic.
+    // See https://github.com/ggez/ggez/issues/579
+    env::set_var("WINIT_UNIX_BACKEND", "x11");
+
+    let (mut context, mut event_loop) =
+        ContextBuilder::new("vndf", "Hanno Braun")
+            .build()?;
+
+    let mut game = Game;
+
+    run(&mut context, &mut event_loop, &mut game)
+}
+
+
+pub struct Game;
+
+impl EventHandler for Game {
+    fn update(&mut self, _: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, _: &mut Context) -> GameResult {
+        Ok(())
+    }
 }
