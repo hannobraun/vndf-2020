9983ec0dbd5951bd01867a9e09c273ff339045d1
Hanno Braun <hb@hannobraun.de>
2019-12-21 10:45:55 +0100

    Set coordinate system in dedicated function

--- src/graphics.rs
+++ src/graphics.rs
@@ -17,26 +17,7 @@ pub struct Graphics {
 
 impl Graphics {
     pub fn new(context: &mut Context) -> GameResult<Self> {
-        let (width, height) = graphics::drawable_size(context);
-        let aspect_ratio = width / height;
-
-        let min_size = 1000.0;
-
-        let size = if aspect_ratio >= 1.0 {
-            [min_size * aspect_ratio, min_size]
-        }
-        else {
-            [min_size, min_size / aspect_ratio]
-        };
-
-        let screen_coordinates = Rect {
-            x: -size[0] / 2.0,
-            y: -size[1] / 2.0,
-            w: size[0],
-            h: size[1],
-        };
-
-        graphics::set_screen_coordinates(context, screen_coordinates)?;
+        set_coordinate_system(context)?;
 
         let ship = Mesh::new_polygon(
             context,
@@ -71,4 +52,30 @@ impl Graphics {
         graphics::present(context)?;
         Ok(())
     }
+}
+
+
+fn set_coordinate_system(context: &mut Context) -> GameResult {
+    let (width, height) = graphics::drawable_size(context);
+    let aspect_ratio = width / height;
+
+    let min_size = 1000.0;
+
+    let size = if aspect_ratio >= 1.0 {
+        [min_size * aspect_ratio, min_size]
+    }
+    else {
+        [min_size, min_size / aspect_ratio]
+    };
+
+    let screen_coordinates = Rect {
+        x: -size[0] / 2.0,
+        y: -size[1] / 2.0,
+        w: size[0],
+        h: size[1],
+    };
+
+    graphics::set_screen_coordinates(context, screen_coordinates)?;
+
+    Ok(())
 }
