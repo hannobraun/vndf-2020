0dba58724f8c17e946d8b8abde54d6e081332303
Hanno Braun <hb@hannobraun.de>
2019-12-22 23:10:18 +0100

    Refactor

--- src/input.rs
+++ src/input.rs
@@ -14,20 +14,8 @@ pub struct Input {
 impl Input {
     pub fn read(context: &mut Context) -> Self {
         Self {
-            rotation: rotation(context),
+            rotation: Rotation::read(context),
         }
-    }
-}
-
-
-pub fn rotation(context: &mut Context) -> Rotation {
-    let left  = is_key_pressed(context, KeyCode::Left);
-    let right = is_key_pressed(context, KeyCode::Right);
-
-    match (left, right) {
-        (true, false) => Rotation::Left,
-        (false, true) => Rotation::Right,
-        _             => Rotation::None,
     }
 }
 
@@ -37,4 +25,17 @@ pub enum Rotation {
     Left  = -1,
     Right = 1,
     None  = 0,
+}
+
+impl Rotation {
+    pub fn read(context: &mut Context) -> Self {
+        let left  = is_key_pressed(context, KeyCode::Left);
+        let right = is_key_pressed(context, KeyCode::Right);
+
+        match (left, right) {
+            (true, false) => Rotation::Left,
+            (false, true) => Rotation::Right,
+            _             => Rotation::None,
+        }
+    }
 }
