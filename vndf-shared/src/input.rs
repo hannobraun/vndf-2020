f841206e3a512a5278430092bfd40f70cc75229f
Hanno Braun <hb@hannobraun.de>
2019-12-26 14:21:35 +0100

    Derive `Serialize`, `Deserialize` for `Event`

--- vndf-shared/src/input.rs
+++ vndf-shared/src/input.rs
@@ -1,10 +1,17 @@
+use serde::{
+    Deserialize,
+    Serialize,
+};
+
+
+#[derive(Deserialize, Serialize)]
 pub enum Event {
     Rotate(Rotation),
     Thrust(bool),
     LaunchMissile,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Deserialize, Serialize)]
 pub enum Rotation {
     Left  = -1,
     Right = 1,
