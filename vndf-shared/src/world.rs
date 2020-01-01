81cb2cf22fe6a19457663a26956397852bcc3190
Hanno Braun <hb@hannobraun.de>
2020-01-01 10:51:04 +0100

    Remove unused code

--- vndf-shared/src/world.rs
+++ vndf-shared/src/world.rs
@@ -1,7 +1,5 @@
 use hecs::{
     self,
-    DynamicBundle,
-    NoSuchEntity,
     QueryBorrow,
 };
 
@@ -11,14 +9,6 @@ pub struct Query<'r>(&'r mut hecs::World);
 impl<'r> Query<'r> {
     pub fn new(inner: &'r mut hecs::World) -> Self {
         Self(inner)
-    }
-
-    pub fn spawn(&mut self, components: impl DynamicBundle) -> Entity {
-        self.0.spawn(components)
-    }
-
-    pub fn despawn(&mut self, entity: Entity) -> Result<(), NoSuchEntity> {
-        self.0.despawn(entity)
     }
 
     pub fn query<Q: hecs::Query>(&self) -> QueryBorrow<Q> {
