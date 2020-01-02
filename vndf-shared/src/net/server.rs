d8ff3b93821c19a3bfc2db2313bcc9aefec16bcc
Hanno Braun <hb@hannobraun.de>
2020-01-02 13:57:49 +0100

    Add method for iterating over all clients

--- vndf-shared/src/net/server.rs
+++ vndf-shared/src/net/server.rs
@@ -81,6 +81,11 @@ impl Server {
         self.addr
     }
 
+    pub fn clients(&self) -> impl Iterator<Item=SocketAddr> + '_ {
+        self.conns.keys()
+            .map(|&addr| addr)
+    }
+
     pub fn send(&mut self, addr: SocketAddr, message: msg::FromServer) {
         let conn = match self.conns.get_mut(&addr) {
             Some(conn) => conn,
