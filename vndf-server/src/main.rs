0aee032912fe0ed62d7387ea4492f225af8b830e
Hanno Braun <hb@hannobraun.de>
2020-01-05 16:11:38 +0100

    Rate-limit updates on the server
    Without this, it's going to completely overwhelm any client that
    connects.

--- vndf-server/src/main.rs
+++ vndf-server/src/main.rs
@@ -1,8 +1,17 @@
 pub use vndf_shared as shared;
 
 
+use std::{
+    time::{
+        Duration,
+        Instant,
+    },
+    thread,
+};
+
 use self::shared::{
     Server,
+    game::FRAME_TIME,
     net,
 };
 
@@ -15,7 +24,20 @@ fn main() -> net::Result {
 
     let mut server = Server::start_default()?;
 
+    let     frame_time  = Duration::from_millis((FRAME_TIME * 1000.0) as u64);
+    let mut last_update = Instant::now();
+
     loop {
+        let now = Instant::now();
+
+        let sleep_time = frame_time
+            .checked_sub(now.duration_since(last_update));
+        if let Some(sleep_time) = sleep_time {
+            thread::sleep(sleep_time);
+        }
+
+        last_update = now;
+
         server.update();
     }
 }
