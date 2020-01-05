bc757f5547bff59ef0de72cdda0c266b913a96e3
Hanno Braun <hb@hannobraun.de>
2020-01-05 15:56:58 +0100

    Enable logging on server

--- vndf-server/src/main.rs
+++ vndf-server/src/main.rs
@@ -8,6 +8,11 @@ use self::shared::{
 
 
 fn main() -> net::Result {
+    env_logger::init_from_env(
+        env_logger::Env::new()
+            .default_filter_or("vndf_shared=info,vndf_server=info")
+    );
+
     let mut server = Server::start_default()?;
 
     loop {
