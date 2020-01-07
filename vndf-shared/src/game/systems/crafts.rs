eb887b2c77d0967720f6a502ba4b755af29b3faa
Hanno Braun <hb@hannobraun.de>
2020-01-07 09:51:59 +0100

    Reorder code

--- vndf-shared/src/game/systems/crafts.rs
+++ vndf-shared/src/game/systems/crafts.rs
@@ -7,15 +7,15 @@ use crate::{
 };
 
 
-pub fn update_engines(world: world::Query, dt: f32) {
-    for (_, (engine, body)) in &mut world.query::<(&mut Engine, &mut Body)>() {
-        engine.update(body, dt)
-    }
-}
-
 pub fn update_bodies(world: world::Query, world_size: f32, dt: f32) {
     for (_, (body,)) in &mut world.query::<(&mut Body,)>() {
         body.update(dt);
         body.enforce_boundary(world_size);
+    }
+}
+
+pub fn update_engines(world: world::Query, dt: f32) {
+    for (_, (engine, body)) in &mut world.query::<(&mut Engine, &mut Body)>() {
+        engine.update(body, dt)
     }
 }
