de6b2725e6ddc8cf23cf32e7dff1d0c2d98931f6
Hanno Braun <hb@hannobraun.de>
2020-01-19 23:10:30 +0100

    Add event stream

--- vndf-shared/src/events.rs
+++ vndf-shared/src/events.rs
@@ -4,6 +4,41 @@ use std::{
 };
 
 
+pub struct Stream<T>(VecDeque<T>);
+
+impl<T> Stream<T> {
+    pub fn new() -> Self {
+        Stream(VecDeque::new())
+    }
+
+    pub fn sink(&mut self) -> Sink<T> {
+        Sink(self)
+    }
+
+    pub fn source(&mut self) -> Source<T> {
+        Source(self)
+    }
+}
+
+
+pub struct Sink<'r, T>(&'r mut Stream<T>);
+
+impl<T> Sink<'_, T> {
+    pub fn push(&mut self, event: T) {
+        (self.0).0.push_back(event);
+    }
+}
+
+
+pub struct Source<'r, T>(&'r mut Stream<T>);
+
+impl<T> Source<'_, T> {
+    pub fn ready(&mut self) -> impl Iterator<Item=T> + '_ {
+        (self.0).0.drain(..)
+    }
+}
+
+
 pub struct Events<T>(VecDeque<T>);
 
 impl<T> Events<T> {
