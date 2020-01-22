368bb064929f712ec3364b656d2eaa76a2937277
Hanno Braun <hb@hannobraun.de>
2020-01-22 22:54:04 +0100

    Add additional CGS infrastructure

--- vndf-shared/src/cgs.rs
+++ vndf-shared/src/cgs.rs
@@ -1,8 +1,10 @@
 use slotmap::{
     DefaultKey,
     DenseSlotMap,
+    SparseSecondaryMap,
 };
 
 
-pub type Handle   = DefaultKey;
-pub type Store<T> = DenseSlotMap<DefaultKey, T>;
+pub type Handle            = DefaultKey;
+pub type Store<T>          = DenseSlotMap<DefaultKey, T>;
+pub type SecondaryStore<T> = SparseSecondaryMap<DefaultKey, T>;
