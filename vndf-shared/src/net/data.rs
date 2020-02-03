db5701204396e16bae14de2ad69c11a4050b4604
Hanno Braun <hb@hannobraun.de>
2020-02-03 21:17:04 +0100

    Generate `Data` in macro

--- vndf-shared/src/net/data.rs
+++ vndf-shared/src/net/data.rs
@@ -22,86 +22,49 @@ use crate::{
 };
 
 
-pub struct Data {
-    pub bodies:     SecondaryStore<Body>,
-    pub crafts:     SecondaryStore<Craft>,
-    pub explosions: SecondaryStore<Explosion>,
-    pub healths:    SecondaryStore<Health>,
-    pub missiles:   SecondaryStore<Missile>,
-    pub positions:  SecondaryStore<Position>,
-    pub ships:      SecondaryStore<Ship>,
-    pub velocities: SecondaryStore<Velocity>,
+macro_rules! data {
+    ($($name:ident, $ty:ident;)*) => {
+        pub struct Data {
+            $(pub $name: SecondaryStore<$ty>,)*
+        }
+
+        impl Data {
+            pub fn new() -> Self {
+                Self {
+                    $($name: SecondaryStore::new(),)*
+                }
+            }
+
+            pub fn update(&mut self, handle: Handle, component: Component) {
+                match component {
+                    $(
+                        Component::$ty(value) => {
+                            self.$name.insert(handle, value);
+                        }
+                    )*
+                }
+            }
+
+            pub fn remove(&mut self, handle: ComponentHandle) {
+                match handle {
+                    $(
+                        ComponentHandle::$ty(handle) => {
+                            self.$name.remove(handle);
+                        }
+                    )*
+                }
+            }
+        }
+    };
 }
 
-impl Data {
-    pub fn new() -> Self {
-        Self {
-            bodies:     SecondaryStore::new(),
-            crafts:     SecondaryStore::new(),
-            explosions: SecondaryStore::new(),
-            healths:    SecondaryStore::new(),
-            missiles:   SecondaryStore::new(),
-            positions:  SecondaryStore::new(),
-            ships:      SecondaryStore::new(),
-            velocities: SecondaryStore::new(),
-        }
-    }
-
-    pub fn update(&mut self, handle: Handle, component: Component) {
-        match component {
-            Component::Body(body) => {
-                self.bodies.insert(handle, body);
-            }
-            Component::Craft(craft) => {
-                self.crafts.insert(handle, craft);
-            }
-            Component::Explosion(explosion) => {
-                self.explosions.insert(handle, explosion);
-            }
-            Component::Health(health) => {
-                self.healths.insert(handle, health);
-            }
-            Component::Missile(missile) => {
-                self.missiles.insert(handle, missile);
-            }
-            Component::Position(position) => {
-                self.positions.insert(handle, position);
-            }
-            Component::Ship(ship) => {
-                self.ships.insert(handle, ship);
-            }
-            Component::Velocity(velocity) => {
-                self.velocities.insert(handle, velocity);
-            }
-        }
-    }
-
-    pub fn remove(&mut self, handle: ComponentHandle) {
-        match handle {
-            ComponentHandle::Body(handle) => {
-                self.bodies.remove(handle);
-            }
-            ComponentHandle::Craft(handle) => {
-                self.crafts.remove(handle);
-            }
-            ComponentHandle::Explosion(handle) => {
-                self.explosions.remove(handle);
-            }
-            ComponentHandle::Health(handle) => {
-                self.healths.remove(handle);
-            }
-            ComponentHandle::Missile(handle) => {
-                self.missiles.remove(handle);
-            }
-            ComponentHandle::Position(handle) => {
-                self.positions.remove(handle);
-            }
-            ComponentHandle::Ship(handle) => {
-                self.ships.remove(handle);
-            }
-            ComponentHandle::Velocity(handle) => {
-                self.velocities.remove(handle);
-            }
-        }
-    }
-}
+data!(
+    bodies,     Body;
+    crafts,     Craft;
+    explosions, Explosion;
+    healths,    Health;
+    missiles,   Missile;
+    positions,  Position;
+    ships,      Ship;
+    velocities, Velocity;
+);
