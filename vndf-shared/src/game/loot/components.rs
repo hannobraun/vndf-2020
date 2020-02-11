4116cdaba9717cde0abe86316f17dde4a19469f1
Hanno Braun <hb@hannobraun.de>
2020-02-11 20:18:58 +0100

    Derive traits for `Loot`

--- vndf-shared/src/game/loot/components.rs
+++ vndf-shared/src/game/loot/components.rs
@@ -1,6 +1,12 @@
+use serde::{
+    Deserialize,
+    Serialize,
+};
+
 use crate::cgs::Handle;
 
 
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq)]
 pub struct Loot {
     pub body:     Handle,
     pub fuel:     f32,
