b9273ee74d200412eb70212ee89ad11fa688f8f1
Hanno Braun <hb@hannobraun.de>
2020-02-13 22:20:47 +0100

    Move handle to dedicated module

---
+++ toadster/src/handle.rs
@@ -0,0 +1,56 @@
+use std::{
+    fmt,
+    hash::{
+        Hash,
+        Hasher,
+    },
+    marker::PhantomData,
+};
+
+use serde::{
+    Deserialize,
+    Serialize,
+};
+use slotmap::DefaultKey;
+
+
+#[derive(Deserialize, Serialize)]
+pub struct Handle<T>(pub(crate) DefaultKey, PhantomData<T>);
+
+impl<T> Handle<T> {
+    pub(crate) fn new(key: DefaultKey) -> Self {
+        Self(key, PhantomData)
+    }
+}
+
+impl<T> Clone for Handle<T> {
+    fn clone(&self) -> Self {
+        Handle::new(self.0.clone())
+    }
+}
+
+impl<T> Copy for Handle<T> {}
+
+impl<T> fmt::Debug for Handle<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Handle<T>(")?;
+        self.0.fmt(f)?;
+        write!(f, ", PhantomData)")?;
+
+        Ok(())
+    }
+}
+
+impl<T> Eq for Handle<T> {}
+
+impl<T> PartialEq for Handle<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl<T> Hash for Handle<T> {
+    fn hash<H>(&self, state: &mut H) where H: Hasher {
+        self.0.hash(state)
+    }
+}
--- toadster/src/lib.rs
+++ toadster/src/lib.rs
@@ -1,69 +1,13 @@
+pub mod handle;
 pub mod secondary_store;
 pub mod store;
 
 
 pub use self::{
+    handle::Handle,
     secondary_store::SecondaryStore,
     store::Store,
 };
-
-
-use std::{
-    fmt,
-    hash::{
-        Hash,
-        Hasher,
-    },
-    marker::PhantomData,
-};
-
-use serde::{
-    Deserialize,
-    Serialize,
-};
-use slotmap::DefaultKey;
-
-
-#[derive(Deserialize, Serialize)]
-pub struct Handle<T>(DefaultKey, PhantomData<T>);
-
-impl<T> Handle<T> {
-    pub(crate) fn new(key: DefaultKey) -> Self {
-        Self(key, PhantomData)
-    }
-}
-
-impl<T> Clone for Handle<T> {
-    fn clone(&self) -> Self {
-        Handle::new(self.0.clone())
-    }
-}
-
-impl<T> Copy for Handle<T> {}
-
-impl<T> fmt::Debug for Handle<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Handle<T>(")?;
-        self.0.fmt(f)?;
-        write!(f, ", PhantomData)")?;
-
-        Ok(())
-    }
-}
-
-impl<T> Eq for Handle<T> {}
-
-impl<T> PartialEq for Handle<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.eq(&other.0)
-    }
-}
-
-impl<T> Hash for Handle<T> {
-    fn hash<H>(&self, state: &mut H) where H: Hasher {
-        self.0.hash(state)
-    }
-}
 
 
 pub trait Get<T> {
