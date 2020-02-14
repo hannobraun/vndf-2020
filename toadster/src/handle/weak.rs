40a87c7acf74faf7a4f7ac610ee2d9d7a024fc4b
Hanno Braun <hb@hannobraun.de>
2020-02-14 15:30:12 +0100

    Implement `Eq` for `handle::Weak`

--- toadster/src/handle/weak.rs
+++ toadster/src/handle/weak.rs
@@ -17,3 +17,11 @@ impl<T> From<&handle::Strong<T>> for Weak<T> {
         Self(handle.key, PhantomData)
     }
 }
+
+impl<T> Eq for Weak<T> {}
+
+impl<T> PartialEq for Weak<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
