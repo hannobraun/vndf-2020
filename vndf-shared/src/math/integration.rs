3b52858d43cb536684c333bd4956aa7a07a48b6d
Hanno Braun <hb@hannobraun.de>
2020-02-24 18:11:08 +0100

    Refactor

--- vndf-shared/src/math/integration.rs
+++ vndf-shared/src/math/integration.rs
@@ -5,6 +5,16 @@ use crate::math::{
 
 
 pub fn integrate(dt: f32, pos: &mut Pnt2, vel: &mut Vec2, acc: &mut Vec2) {
+    semi_implicit_euler(dt, pos, vel, acc)
+}
+
+/// Semi-implicit Euler method
+pub fn semi_implicit_euler(
+    dt:  f32,
+    pos: &mut Pnt2,
+    vel: &mut Vec2,
+    acc: &mut Vec2,
+) {
     *vel += *acc * dt;
     *pos += *vel * dt;
 }
