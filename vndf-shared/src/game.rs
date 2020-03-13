551b76e1021ba54fa70af29100d12ea73df38d9b
Hanno Braun <hb@hannobraun.de>
2020-03-13 10:26:33 +0100

    Refactor

--- vndf-shared/src/game.rs
+++ vndf-shared/src/game.rs
@@ -53,7 +53,6 @@ pub struct State {
     loot:       loot::Feature,
     missiles:   missiles::Feature,
     physics:    physics::Feature,
-    planets:    planets::Feature,
     players:    players::Feature,
     ships:      ships::Feature,
 }
@@ -77,7 +76,6 @@ impl State {
             loot:       loot::Feature::new(),
             missiles:   missiles::Feature::new(),
             physics:    physics::Feature::new(),
-            planets:    planets::Feature::new(),
             players:    players::Feature::new(),
             ships:      ships::Feature::new(),
         }
@@ -112,12 +110,15 @@ impl State {
                 &event,
                 &mut self.data.explosions,
             );
-            self.planets.on_update(
+
+            let planets = planets::Feature::new();
+            planets.on_update(
                 &mut self.data.bodies,
                 &mut self.data.healths,
                 &self.data.planets,
                 &self.data.positions,
             );
+
             self.physics.on_update(
                 &event,
                 WORLD_SIZE,
