2a958a85770988808fc251979399656a537d53a9
Hanno Braun <hb@hannobraun.de>
2020-03-13 10:35:19 +0100

    Convert functions into associated functions

--- vndf-shared/src/game/planets.rs
+++ vndf-shared/src/game/planets.rs
@@ -1,10 +1,8 @@
 pub mod components;
 pub mod feature;
-pub mod systems;
 
 
 pub use self::{
     components::*,
     feature::*,
-    systems::*,
 };
--- vndf-shared/src/game/planets/feature.rs
+++ vndf-shared/src/game/planets/feature.rs
@@ -8,13 +8,10 @@ use crate::{
             Position,
         },
     },
+    math::prelude::*,
 };
 
-use super::{
-    Planet,
-    apply_gravitation,
-    check_collision,
-};
+use super::Planet;
 
 
 pub struct Feature<'r> {
@@ -26,16 +23,55 @@ pub struct Feature<'r> {
 
 impl Feature<'_> {
     pub fn on_update(&mut self) {
-        apply_gravitation(
+        Self::apply_gravitation(
             self.bodies,
             self.planets,
             self.positions,
         );
-        check_collision(
+        Self::check_collision(
             self.bodies,
             self.healths,
             self.planets,
             self.positions,
         );
+    }
+
+    pub fn apply_gravitation(
+        bodies:    &mut impl for<'r> store::ValuesMut<'r, Body>,
+        planets:   &impl for<'r> store::Values<'r, Planet>,
+        positions: impl store::Get<Position>,
+    )
+        -> Option<()>
+    {
+        let bodies  = bodies.values_mut();
+        let planets = planets.values();
+
+        for (body, planet) in bodies.zip(planets) {
+            planet.apply_gravitation(body, &positions);
+        }
+
+        Some(())
+    }
+
+    pub fn check_collision(
+        bodies:    &store::Strong<Body>,
+        healths:   &mut store::Strong<Health>,
+        planets:   &store::Strong<Planet>,
+        positions: &store::Strong<Position>,
+    )
+        -> Option<()>
+    {
+        for planet in planets.values() {
+            for health in healths.values_mut() {
+                let body = bodies.get(&health.body)?;
+                let pos  = positions.get(&body.pos)?;
+
+                if pos.0.distance(planet.pos) <= planet.size {
+                    health.value = 0.0;
+                }
+            }
+        }
+
+        Some(())
     }
 }
