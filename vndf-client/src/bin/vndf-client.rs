f4275083b1b20044a915f3cf8107ae7119abc029
Hanno Braun <hb@hannobraun.de>
2020-03-18 08:57:14 +0100

    Add error type to client binary

--- vndf-client/src/bin/vndf-client.rs
+++ vndf-client/src/bin/vndf-client.rs
@@ -2,18 +2,30 @@ use std::thread;
 
 use vndf_client as client;
 use vndf_server::server::Server;
-use vndf_shared::main_loop::main_loop;
+use vndf_shared::{
+    main_loop::main_loop,
+    net,
+};
 
 
-fn main() -> Result<(), client::Error> {
+fn main() -> Result<(), Error> {
     env_logger::init_from_env(
         env_logger::Env::new()
             .default_filter_or("vndf_shared=info,vndf_client=info")
     );
 
-    let mut server = Server::start_local()?;
+    let mut server = Server::start_local()
+        .map_err(|err| Error::Init(err))?;
     let     addr   = server.addr();
 
     thread::spawn(move || main_loop(|| server.update()));
     client::start(addr)
+        .map_err(|err| Error::Run(err))
+}
+
+
+#[derive(Debug)]
+pub enum Error {
+    Init(net::Error),
+    Run(client::Error),
 }
