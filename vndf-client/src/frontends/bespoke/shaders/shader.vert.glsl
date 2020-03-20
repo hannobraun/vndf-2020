9e078756c4a913ae294cfca280ac9546f82538ab
Hanno Braun <hb@hannobraun.de>
2020-03-20 11:08:08 +0100

    Add comments to shaders

--- vndf-client/src/frontends/bespoke/shaders/shader.vert.glsl
+++ vndf-client/src/frontends/bespoke/shaders/shader.vert.glsl
@@ -1,3 +1,7 @@
+// ATTENTION:
+// Changes to this file will have no effect on the shader used in the game. You
+// need to compile this file using `compile-shaders.sh`, to see any effect.
+
 #version 450
 
 void main() {}
