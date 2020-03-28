cd700cf92259f24077a371ae9a8790aeda83ff71
Hanno Braun <hb@hannobraun.de>
2020-03-28 12:21:23 +0100

    Improve macro
    The new arguments should make the macro a bit more flexible, allowing
    additions that weren't possible to make before.

--- vndf-client/src/game/config.rs
+++ vndf-client/src/game/config.rs
@@ -111,14 +111,14 @@ pub enum Key {
 }
 
 macro_rules! keys {
-    ($($s:tt => $k:tt,)*) => {
+    ($($s:tt, $type:ident, $type2:ident, $key:ident;)*) => {
         impl Serialize for Key {
             fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
                 where S: Serializer
             {
                 #[allow(unused_parens)]
                 let expr = match self {
-                    $($k => $s,)*
+                    $(Key::$type($type2::$key) => $s,)*
 
                     key => panic!("Variant not allowed: {:?}", key),
                 };
@@ -150,7 +150,7 @@ macro_rules! keys {
             {
                 #[allow(unused_parens)]
                 match value {
-                    $($s => Ok($k),)*
+                    $($s => Ok(Key::$type($type2::$key)),)*
 
                     _ =>
                         Err(
@@ -166,7 +166,7 @@ macro_rules! keys {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
                 #[allow(unused_parens)]
                 match self {
-                    $($k => write!(f, "{}", $s),)*
+                    $(Key::$type($type2::$key) => write!(f, "{}", $s),)*
 
                     _ => write!(f, "Unknown key"),
                 }
@@ -176,170 +176,171 @@ macro_rules! keys {
 }
 
 keys!(
-    "Key1"             => (Key::Keyboard(KeyCode::Key1)),
-    "Key2"             => (Key::Keyboard(KeyCode::Key2)),
-    "Key3"             => (Key::Keyboard(KeyCode::Key3)),
-    "Key4"             => (Key::Keyboard(KeyCode::Key4)),
-    "Key5"             => (Key::Keyboard(KeyCode::Key5)),
-    "Key6"             => (Key::Keyboard(KeyCode::Key6)),
-    "Key7"             => (Key::Keyboard(KeyCode::Key7)),
-    "Key8"             => (Key::Keyboard(KeyCode::Key8)),
-    "Key9"             => (Key::Keyboard(KeyCode::Key9)),
-    "Key0"             => (Key::Keyboard(KeyCode::Key0)),
-    "A"                => (Key::Keyboard(KeyCode::A)),
-    "B"                => (Key::Keyboard(KeyCode::B)),
-    "C"                => (Key::Keyboard(KeyCode::C)),
-    "D"                => (Key::Keyboard(KeyCode::D)),
-    "E"                => (Key::Keyboard(KeyCode::E)),
-    "F"                => (Key::Keyboard(KeyCode::F)),
-    "G"                => (Key::Keyboard(KeyCode::G)),
-    "H"                => (Key::Keyboard(KeyCode::H)),
-    "I"                => (Key::Keyboard(KeyCode::I)),
-    "J"                => (Key::Keyboard(KeyCode::J)),
-    "K"                => (Key::Keyboard(KeyCode::K)),
-    "L"                => (Key::Keyboard(KeyCode::L)),
-    "M"                => (Key::Keyboard(KeyCode::M)),
-    "N"                => (Key::Keyboard(KeyCode::N)),
-    "O"                => (Key::Keyboard(KeyCode::O)),
-    "P"                => (Key::Keyboard(KeyCode::P)),
-    "Q"                => (Key::Keyboard(KeyCode::Q)),
-    "R"                => (Key::Keyboard(KeyCode::R)),
-    "S"                => (Key::Keyboard(KeyCode::S)),
-    "T"                => (Key::Keyboard(KeyCode::T)),
-    "U"                => (Key::Keyboard(KeyCode::U)),
-    "V"                => (Key::Keyboard(KeyCode::V)),
-    "W"                => (Key::Keyboard(KeyCode::W)),
-    "X"                => (Key::Keyboard(KeyCode::X)),
-    "Y"                => (Key::Keyboard(KeyCode::Y)),
-    "Z"                => (Key::Keyboard(KeyCode::Z)),
-    "Esc"              => (Key::Keyboard(KeyCode::Escape)),
-    "F1"               => (Key::Keyboard(KeyCode::F1)),
-    "F2"               => (Key::Keyboard(KeyCode::F2)),
-    "F3"               => (Key::Keyboard(KeyCode::F3)),
-    "F4"               => (Key::Keyboard(KeyCode::F4)),
-    "F5"               => (Key::Keyboard(KeyCode::F5)),
-    "F6"               => (Key::Keyboard(KeyCode::F6)),
-    "F7"               => (Key::Keyboard(KeyCode::F7)),
-    "F8"               => (Key::Keyboard(KeyCode::F8)),
-    "F9"               => (Key::Keyboard(KeyCode::F9)),
-    "F10"              => (Key::Keyboard(KeyCode::F10)),
-    "F11"              => (Key::Keyboard(KeyCode::F11)),
-    "F12"              => (Key::Keyboard(KeyCode::F12)),
-    "F13"              => (Key::Keyboard(KeyCode::F13)),
-    "F14"              => (Key::Keyboard(KeyCode::F14)),
-    "F15"              => (Key::Keyboard(KeyCode::F15)),
-    "F16"              => (Key::Keyboard(KeyCode::F16)),
-    "F17"              => (Key::Keyboard(KeyCode::F17)),
-    "F18"              => (Key::Keyboard(KeyCode::F18)),
-    "F19"              => (Key::Keyboard(KeyCode::F19)),
-    "F20"              => (Key::Keyboard(KeyCode::F20)),
-    "F21"              => (Key::Keyboard(KeyCode::F21)),
-    "F22"              => (Key::Keyboard(KeyCode::F22)),
-    "F23"              => (Key::Keyboard(KeyCode::F23)),
-    "F24"              => (Key::Keyboard(KeyCode::F24)),
-    "Snapshot"         => (Key::Keyboard(KeyCode::Snapshot)),
-    "Scroll"           => (Key::Keyboard(KeyCode::Scroll)),
-    "Pause"            => (Key::Keyboard(KeyCode::Pause)),
-    "Insert"           => (Key::Keyboard(KeyCode::Insert)),
-    "Home"             => (Key::Keyboard(KeyCode::Home)),
-    "Delete"           => (Key::Keyboard(KeyCode::Delete)),
-    "End"              => (Key::Keyboard(KeyCode::End)),
-    "PageDown"         => (Key::Keyboard(KeyCode::PageDown)),
-    "PageUp"           => (Key::Keyboard(KeyCode::PageUp)),
-    "Left"             => (Key::Keyboard(KeyCode::Left)),
-    "Up"               => (Key::Keyboard(KeyCode::Up)),
-    "Right"            => (Key::Keyboard(KeyCode::Right)),
-    "Down"             => (Key::Keyboard(KeyCode::Down)),
-    "Back"             => (Key::Keyboard(KeyCode::Back)),
-    "Return"           => (Key::Keyboard(KeyCode::Return)),
-    "Space"            => (Key::Keyboard(KeyCode::Space)),
-    "Compose"          => (Key::Keyboard(KeyCode::Compose)),
-    "Caret"            => (Key::Keyboard(KeyCode::Caret)),
-    "Numlock"          => (Key::Keyboard(KeyCode::Numlock)),
-    "Numpad0"          => (Key::Keyboard(KeyCode::Numpad0)),
-    "Numpad1"          => (Key::Keyboard(KeyCode::Numpad1)),
-    "Numpad2"          => (Key::Keyboard(KeyCode::Numpad2)),
-    "Numpad3"          => (Key::Keyboard(KeyCode::Numpad3)),
-    "Numpad4"          => (Key::Keyboard(KeyCode::Numpad4)),
-    "Numpad5"          => (Key::Keyboard(KeyCode::Numpad5)),
-    "Numpad6"          => (Key::Keyboard(KeyCode::Numpad6)),
-    "Numpad7"          => (Key::Keyboard(KeyCode::Numpad7)),
-    "Numpad8"          => (Key::Keyboard(KeyCode::Numpad8)),
-    "Numpad9"          => (Key::Keyboard(KeyCode::Numpad9)),
-    "AbntC1"           => (Key::Keyboard(KeyCode::AbntC1)),
-    "AbntC2"           => (Key::Keyboard(KeyCode::AbntC2)),
-    "Add"              => (Key::Keyboard(KeyCode::Add)),
-    "Apostrophe"       => (Key::Keyboard(KeyCode::Apostrophe)),
-    "Apps"             => (Key::Keyboard(KeyCode::Apps)),
-    "At"               => (Key::Keyboard(KeyCode::At)),
-    "Ax"               => (Key::Keyboard(KeyCode::Ax)),
-    "Backslash"        => (Key::Keyboard(KeyCode::Backslash)),
-    "Calculator"       => (Key::Keyboard(KeyCode::Calculator)),
-    "Capital"          => (Key::Keyboard(KeyCode::Capital)),
-    "Colon"            => (Key::Keyboard(KeyCode::Colon)),
-    "Comma"            => (Key::Keyboard(KeyCode::Comma)),
-    "Convert"          => (Key::Keyboard(KeyCode::Convert)),
-    "Decimal"          => (Key::Keyboard(KeyCode::Decimal)),
-    "Divide"           => (Key::Keyboard(KeyCode::Divide)),
-    "Equals"           => (Key::Keyboard(KeyCode::Equals)),
-    "Grave"            => (Key::Keyboard(KeyCode::Grave)),
-    "Kana"             => (Key::Keyboard(KeyCode::Kana)),
-    "Kanji"            => (Key::Keyboard(KeyCode::Kanji)),
-    "LAlt"             => (Key::Keyboard(KeyCode::LAlt)),
-    "LBracket"         => (Key::Keyboard(KeyCode::LBracket)),
-    "LControl"         => (Key::Keyboard(KeyCode::LControl)),
-    "LShift"           => (Key::Keyboard(KeyCode::LShift)),
-    "LWin"             => (Key::Keyboard(KeyCode::LWin)),
-    "Mail"             => (Key::Keyboard(KeyCode::Mail)),
-    "MediaSelect"      => (Key::Keyboard(KeyCode::MediaSelect)),
-    "MediaStop"        => (Key::Keyboard(KeyCode::MediaStop)),
-    "Minus"            => (Key::Keyboard(KeyCode::Minus)),
-    "Multiply"         => (Key::Keyboard(KeyCode::Multiply)),
-    "Mute"             => (Key::Keyboard(KeyCode::Mute)),
-    "MyComputer"       => (Key::Keyboard(KeyCode::MyComputer)),
-    "NavigateForward"  => (Key::Keyboard(KeyCode::NavigateForward)),
-    "NavigateBackward" => (Key::Keyboard(KeyCode::NavigateBackward)),
-    "NextTrack"        => (Key::Keyboard(KeyCode::NextTrack)),
-    "NoConvert"        => (Key::Keyboard(KeyCode::NoConvert)),
-    "NumpadComma"      => (Key::Keyboard(KeyCode::NumpadComma)),
-    "NumpadEnter"      => (Key::Keyboard(KeyCode::NumpadEnter)),
-    "NumpadEquals"     => (Key::Keyboard(KeyCode::NumpadEquals)),
-    "OEM102"           => (Key::Keyboard(KeyCode::OEM102)),
-    "Period"           => (Key::Keyboard(KeyCode::Period)),
-    "PlayPause"        => (Key::Keyboard(KeyCode::PlayPause)),
-    "Power"            => (Key::Keyboard(KeyCode::Power)),
-    "PrevTrack"        => (Key::Keyboard(KeyCode::PrevTrack)),
-    "RAlt"             => (Key::Keyboard(KeyCode::RAlt)),
-    "RBracket"         => (Key::Keyboard(KeyCode::RBracket)),
-    "RControl"         => (Key::Keyboard(KeyCode::RControl)),
-    "RShift"           => (Key::Keyboard(KeyCode::RShift)),
-    "RWin"             => (Key::Keyboard(KeyCode::RWin)),
-    "Semicolon"        => (Key::Keyboard(KeyCode::Semicolon)),
-    "Slash"            => (Key::Keyboard(KeyCode::Slash)),
-    "Sleep"            => (Key::Keyboard(KeyCode::Sleep)),
-    "Stop"             => (Key::Keyboard(KeyCode::Stop)),
-    "Subtract"         => (Key::Keyboard(KeyCode::Subtract)),
-    "Sysrq"            => (Key::Keyboard(KeyCode::Sysrq)),
-    "Tab"              => (Key::Keyboard(KeyCode::Tab)),
-    "Underline"        => (Key::Keyboard(KeyCode::Underline)),
-    "Unlabeled"        => (Key::Keyboard(KeyCode::Unlabeled)),
-    "VolumeDown"       => (Key::Keyboard(KeyCode::VolumeDown)),
-    "VolumeUp"         => (Key::Keyboard(KeyCode::VolumeUp)),
-    "Wake"             => (Key::Keyboard(KeyCode::Wake)),
-    "WebBack"          => (Key::Keyboard(KeyCode::WebBack)),
-    "WebFavorites"     => (Key::Keyboard(KeyCode::WebFavorites)),
-    "WebForward"       => (Key::Keyboard(KeyCode::WebForward)),
-    "WebHome"          => (Key::Keyboard(KeyCode::WebHome)),
-    "WebRefresh"       => (Key::Keyboard(KeyCode::WebRefresh)),
-    "WebSearch"        => (Key::Keyboard(KeyCode::WebSearch)),
-    "WebStop"          => (Key::Keyboard(KeyCode::WebStop)),
-    "Yen"              => (Key::Keyboard(KeyCode::Yen)),
-    "Copy"             => (Key::Keyboard(KeyCode::Copy)),
-    "Paste"            => (Key::Keyboard(KeyCode::Paste)),
-    "Cut"              => (Key::Keyboard(KeyCode::Cut)),
-    "MouseLeft"        => (Key::Mouse(MouseButton::Left)),
-    "MouseRight"       => (Key::Mouse(MouseButton::Right)),
-    "MouseMiddle"      => (Key::Mouse(MouseButton::Middle)),
+    "Key1",             Keyboard, KeyCode, Key1;
+    "Key2",             Keyboard, KeyCode, Key2;
+    "Key3",             Keyboard, KeyCode, Key3;
+    "Key4",             Keyboard, KeyCode, Key4;
+    "Key5",             Keyboard, KeyCode, Key5;
+    "Key6",             Keyboard, KeyCode, Key6;
+    "Key7",             Keyboard, KeyCode, Key7;
+    "Key8",             Keyboard, KeyCode, Key8;
+    "Key9",             Keyboard, KeyCode, Key9;
+    "Key0",             Keyboard, KeyCode, Key0;
+    "A",                Keyboard, KeyCode, A;
+    "B",                Keyboard, KeyCode, B;
+    "C",                Keyboard, KeyCode, C;
+    "D",                Keyboard, KeyCode, D;
+    "E",                Keyboard, KeyCode, E;
+    "F",                Keyboard, KeyCode, F;
+    "G",                Keyboard, KeyCode, G;
+    "H",                Keyboard, KeyCode, H;
+    "I",                Keyboard, KeyCode, I;
+    "J",                Keyboard, KeyCode, J;
+    "K",                Keyboard, KeyCode, K;
+    "L",                Keyboard, KeyCode, L;
+    "M",                Keyboard, KeyCode, M;
+    "N",                Keyboard, KeyCode, N;
+    "O",                Keyboard, KeyCode, O;
+    "P",                Keyboard, KeyCode, P;
+    "Q",                Keyboard, KeyCode, Q;
+    "R",                Keyboard, KeyCode, R;
+    "S",                Keyboard, KeyCode, S;
+    "T",                Keyboard, KeyCode, T;
+    "U",                Keyboard, KeyCode, U;
+    "V",                Keyboard, KeyCode, V;
+    "W",                Keyboard, KeyCode, W;
+    "X",                Keyboard, KeyCode, X;
+    "Y",                Keyboard, KeyCode, Y;
+    "Z",                Keyboard, KeyCode, Z;
+    "Esc",              Keyboard, KeyCode, Escape;
+    "F1",               Keyboard, KeyCode, F1;
+    "F2",               Keyboard, KeyCode, F2;
+    "F3",               Keyboard, KeyCode, F3;
+    "F4",               Keyboard, KeyCode, F4;
+    "F5",               Keyboard, KeyCode, F5;
+    "F6",               Keyboard, KeyCode, F6;
+    "F7",               Keyboard, KeyCode, F7;
+    "F8",               Keyboard, KeyCode, F8;
+    "F9",               Keyboard, KeyCode, F9;
+    "F10",              Keyboard, KeyCode, F10;
+    "F11",              Keyboard, KeyCode, F11;
+    "F12",              Keyboard, KeyCode, F12;
+    "F13",              Keyboard, KeyCode, F13;
+    "F14",              Keyboard, KeyCode, F14;
+    "F15",              Keyboard, KeyCode, F15;
+    "F16",              Keyboard, KeyCode, F16;
+    "F17",              Keyboard, KeyCode, F17;
+    "F18",              Keyboard, KeyCode, F18;
+    "F19",              Keyboard, KeyCode, F19;
+    "F20",              Keyboard, KeyCode, F20;
+    "F21",              Keyboard, KeyCode, F21;
+    "F22",              Keyboard, KeyCode, F22;
+    "F23",              Keyboard, KeyCode, F23;
+    "F24",              Keyboard, KeyCode, F24;
+    "Snapshot",         Keyboard, KeyCode, Snapshot;
+    "Scroll",           Keyboard, KeyCode, Scroll;
+    "Pause",            Keyboard, KeyCode, Pause;
+    "Insert",           Keyboard, KeyCode, Insert;
+    "Home",             Keyboard, KeyCode, Home;
+    "Delete",           Keyboard, KeyCode, Delete;
+    "End",              Keyboard, KeyCode, End;
+    "PageDown",         Keyboard, KeyCode, PageDown;
+    "PageUp",           Keyboard, KeyCode, PageUp;
+    "Left",             Keyboard, KeyCode, Left;
+    "Up",               Keyboard, KeyCode, Up;
+    "Right",            Keyboard, KeyCode, Right;
+    "Down",             Keyboard, KeyCode, Down;
+    "Back",             Keyboard, KeyCode, Back;
+    "Return",           Keyboard, KeyCode, Return;
+    "Space",            Keyboard, KeyCode, Space;
+    "Compose",          Keyboard, KeyCode, Compose;
+    "Caret",            Keyboard, KeyCode, Caret;
+    "Numlock",          Keyboard, KeyCode, Numlock;
+    "Numpad0",          Keyboard, KeyCode, Numpad0;
+    "Numpad1",          Keyboard, KeyCode, Numpad1;
+    "Numpad2",          Keyboard, KeyCode, Numpad2;
+    "Numpad3",          Keyboard, KeyCode, Numpad3;
+    "Numpad4",          Keyboard, KeyCode, Numpad4;
+    "Numpad5",          Keyboard, KeyCode, Numpad5;
+    "Numpad6",          Keyboard, KeyCode, Numpad6;
+    "Numpad7",          Keyboard, KeyCode, Numpad7;
+    "Numpad8",          Keyboard, KeyCode, Numpad8;
+    "Numpad9",          Keyboard, KeyCode, Numpad9;
+    "AbntC1",           Keyboard, KeyCode, AbntC1;
+    "AbntC2",           Keyboard, KeyCode, AbntC2;
+    "Add",              Keyboard, KeyCode, Add;
+    "Apostrophe",       Keyboard, KeyCode, Apostrophe;
+    "Apps",             Keyboard, KeyCode, Apps;
+    "At",               Keyboard, KeyCode, At;
+    "Ax",               Keyboard, KeyCode, Ax;
+    "Backslash",        Keyboard, KeyCode, Backslash;
+    "Calculator",       Keyboard, KeyCode, Calculator;
+    "Capital",          Keyboard, KeyCode, Capital;
+    "Colon",            Keyboard, KeyCode, Colon;
+    "Comma",            Keyboard, KeyCode, Comma;
+    "Convert",          Keyboard, KeyCode, Convert;
+    "Decimal",          Keyboard, KeyCode, Decimal;
+    "Divide",           Keyboard, KeyCode, Divide;
+    "Equals",           Keyboard, KeyCode, Equals;
+    "Grave",            Keyboard, KeyCode, Grave;
+    "Kana",             Keyboard, KeyCode, Kana;
+    "Kanji",            Keyboard, KeyCode, Kanji;
+    "LAlt",             Keyboard, KeyCode, LAlt;
+    "LBracket",         Keyboard, KeyCode, LBracket;
+    "LControl",         Keyboard, KeyCode, LControl;
+    "LShift",           Keyboard, KeyCode, LShift;
+    "LWin",             Keyboard, KeyCode, LWin;
+    "Mail",             Keyboard, KeyCode, Mail;
+    "MediaSelect",      Keyboard, KeyCode, MediaSelect;
+    "MediaStop",        Keyboard, KeyCode, MediaStop;
+    "Minus",            Keyboard, KeyCode, Minus;
+    "Multiply",         Keyboard, KeyCode, Multiply;
+    "Mute",             Keyboard, KeyCode, Mute;
+    "MyComputer",       Keyboard, KeyCode, MyComputer;
+    "NavigateForward",  Keyboard, KeyCode, NavigateForward;
+    "NavigateBackward", Keyboard, KeyCode, NavigateBackward;
+    "NextTrack",        Keyboard, KeyCode, NextTrack;
+    "NoConvert",        Keyboard, KeyCode, NoConvert;
+    "NumpadComma",      Keyboard, KeyCode, NumpadComma;
+    "NumpadEnter",      Keyboard, KeyCode, NumpadEnter;
+    "NumpadEquals",     Keyboard, KeyCode, NumpadEquals;
+    "OEM102",           Keyboard, KeyCode, OEM102;
+    "Period",           Keyboard, KeyCode, Period;
+    "PlayPause",        Keyboard, KeyCode, PlayPause;
+    "Power",            Keyboard, KeyCode, Power;
+    "PrevTrack",        Keyboard, KeyCode, PrevTrack;
+    "RAlt",             Keyboard, KeyCode, RAlt;
+    "RBracket",         Keyboard, KeyCode, RBracket;
+    "RControl",         Keyboard, KeyCode, RControl;
+    "RShift",           Keyboard, KeyCode, RShift;
+    "RWin",             Keyboard, KeyCode, RWin;
+    "Semicolon",        Keyboard, KeyCode, Semicolon;
+    "Slash",            Keyboard, KeyCode, Slash;
+    "Sleep",            Keyboard, KeyCode, Sleep;
+    "Stop",             Keyboard, KeyCode, Stop;
+    "Subtract",         Keyboard, KeyCode, Subtract;
+    "Sysrq",            Keyboard, KeyCode, Sysrq;
+    "Tab",              Keyboard, KeyCode, Tab;
+    "Underline",        Keyboard, KeyCode, Underline;
+    "Unlabeled",        Keyboard, KeyCode, Unlabeled;
+    "VolumeDown",       Keyboard, KeyCode, VolumeDown;
+    "VolumeUp",         Keyboard, KeyCode, VolumeUp;
+    "Wake",             Keyboard, KeyCode, Wake;
+    "WebBack",          Keyboard, KeyCode, WebBack;
+    "WebFavorites",     Keyboard, KeyCode, WebFavorites;
+    "WebForward",       Keyboard, KeyCode, WebForward;
+    "WebHome",          Keyboard, KeyCode, WebHome;
+    "WebRefresh",       Keyboard, KeyCode, WebRefresh;
+    "WebSearch",        Keyboard, KeyCode, WebSearch;
+    "WebStop",          Keyboard, KeyCode, WebStop;
+    "Yen",              Keyboard, KeyCode, Yen;
+    "Copy",             Keyboard, KeyCode, Copy;
+    "Paste",            Keyboard, KeyCode, Paste;
+    "Cut",              Keyboard, KeyCode, Cut;
+
+    "MouseLeft",   Mouse, MouseButton, Left;
+    "MouseRight",  Mouse, MouseButton, Right;
+    "MouseMiddle", Mouse, MouseButton, Middle;
 );
 
 
