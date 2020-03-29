8700caffeff3b56bfc7f9ebf6579a069b902ec54
Hanno Braun <hb@hannobraun.de>
2020-03-29 16:02:40 +0200

    Add support for keyboard input to bespoke frontend

--- vndf-client/src/frontends/bespoke.rs
+++ vndf-client/src/frontends/bespoke.rs
@@ -5,6 +5,7 @@ use std::io::{
 
 use winit::{
     event::{
+        ElementState,
         Event,
         WindowEvent,
     },
@@ -16,12 +17,21 @@ use winit::{
 };
 
 use crate::{
-    game::Game,
+    game::{
+        Game,
+        config::Key,
+        coords::Screen,
+        input::{
+            Input,
+            Transition,
+        },
+    },
     graphics,
+    shared::math::Vec2,
 };
 
 
-pub fn start(_: Game) -> Result<(), Error> {
+pub fn start(mut game: Game) -> Result<(), Error> {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Von Neumann Defense Force")
@@ -129,12 +139,17 @@ pub fn start(_: Game) -> Result<(), Error> {
         &swap_chain_descriptor,
     );
 
+    let mut screen_size = Screen(Vec2::new(0.0, 0.0));
+
     event_loop.run(move |event, _, control_flow| {
         match event {
             Event::MainEventsCleared => {
                 window.request_redraw()
             }
             Event::WindowEvent { event: WindowEvent::Resized(size), .. } => {
+                screen_size.0.x = size.width  as f32;
+                screen_size.0.y = size.height as f32;
+
                 swap_chain_descriptor.width  = size.width;
                 swap_chain_descriptor.height = size.height;
 
@@ -174,6 +189,32 @@ pub fn start(_: Game) -> Result<(), Error> {
             }
             Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
                 *control_flow = ControlFlow::Exit
+            }
+            Event::WindowEvent { event, .. } => {
+                match event {
+                    WindowEvent::KeyboardInput { input, .. } => {
+                        if let Some(key) = input.virtual_keycode {
+                            let key = Key::Keyboard(key);
+
+                            let input = match input.state {
+                                ElementState::Pressed  => Input::KeyDown(key),
+                                ElementState::Released => Input::KeyUp(key),
+                            };
+
+                            let trans = game.input.handle(
+                                input,
+                                &game.state.camera,
+                                screen_size,
+                                &mut game.events,
+                            );
+
+                            if let Transition::Quit = trans {
+                                *control_flow = ControlFlow::Exit
+                            }
+                        }
+                    },
+                    _ => (),
+                }
             }
             _ => {}
         }
