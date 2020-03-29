ab27fa3b74fa97326eac2d14a07408874a6fa89e
Hanno Braun <hb@hannobraun.de>
2020-03-29 19:48:01 +0200

    Refactor

--- vndf-client/src/frontends/bespoke.rs
+++ vndf-client/src/frontends/bespoke.rs
@@ -1,10 +1,6 @@
+mod renderer;
 mod window;
 
-
-use std::io::{
-    self,
-    Cursor,
-};
 
 use winit::{
     event::{
@@ -32,7 +28,10 @@ use crate::{
     shared::math::Vec2,
 };
 
-use self::window::Window;
+use self::{
+    renderer::Renderer,
+    window::Window,
+};
 
 
 pub fn start(mut game: Game) -> Result<(), Error> {
@@ -40,104 +39,10 @@ pub fn start(mut game: Game) -> Result<(), Error> {
     let window = Window::new(&event_loop)
         .map_err(|err| Error::Winit(err))?;
 
-    let size    = window.0.inner_size();
-    let surface = wgpu::Surface::create(&window.0);
+    let mut renderer = Renderer::new(&window)
+        .map_err(|err| Error::Renderer(err))?;
 
-    let adapter =
-        wgpu::Adapter::request(
-            &wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::Default,
-                backends:         wgpu::BackendBit::all(),
-            },
-        )
-        .ok_or(Error::AdapterRequest)?;
-
-    let (device, mut queue) = adapter.request_device(
-        &wgpu::DeviceDescriptor {
-            extensions: wgpu::Extensions { anisotropic_filtering: false },
-            limits:     wgpu::Limits::default(),
-        },
-    );
-
-    let vertex_shader = include_bytes!("bespoke/shaders/shader.vert.spv");
-    let vertex_module_module = device.create_shader_module(
-        &wgpu::read_spirv(Cursor::new(&vertex_shader[..]))?,
-    );
-
-    let fragment_shader = include_bytes!("bespoke/shaders/shader.frag.spv");
-    let fragment_module_module = device.create_shader_module(
-        &wgpu::read_spirv(Cursor::new(&fragment_shader[..]))?,
-    );
-
-    let bind_group_layout = device.create_bind_group_layout(
-        &wgpu::BindGroupLayoutDescriptor { bindings: &[] },
-    );
-    let bind_group = device.create_bind_group(
-        &wgpu::BindGroupDescriptor {
-            layout:   &bind_group_layout,
-            bindings: &[],
-        },
-    );
-    let pipeline_layout = device.create_pipeline_layout(
-        &wgpu::PipelineLayoutDescriptor {
-            bind_group_layouts: &[&bind_group_layout],
-        },
-    );
-
-    let render_pipeline = device.create_render_pipeline(
-        &wgpu::RenderPipelineDescriptor {
-            layout: &pipeline_layout,
-            vertex_stage: wgpu::ProgrammableStageDescriptor {
-                module:      &vertex_module_module,
-                entry_point: "main",
-            },
-            fragment_stage: Some(
-                wgpu::ProgrammableStageDescriptor {
-                    module:      &fragment_module_module,
-                    entry_point: "main",
-                }
-            ),
-            rasterization_state: Some(
-                wgpu::RasterizationStateDescriptor {
-                    front_face:             wgpu::FrontFace::Ccw,
-                    cull_mode:              wgpu::CullMode::None,
-                    depth_bias:             0,
-                    depth_bias_slope_scale: 0.0,
-                    depth_bias_clamp:       0.0,
-                }
-            ),
-            primitive_topology: wgpu::PrimitiveTopology::TriangleStrip,
-            color_states: &[
-                wgpu::ColorStateDescriptor {
-                    format:      wgpu::TextureFormat::Bgra8UnormSrgb,
-                    color_blend: wgpu::BlendDescriptor::REPLACE,
-                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
-                    write_mask:  wgpu::ColorWrite::ALL,
-                },
-            ],
-
-            depth_stencil_state:       None,
-            index_format:              wgpu::IndexFormat::Uint16,
-            vertex_buffers:            &[],
-            sample_count:              1,
-            sample_mask:               !0,
-            alpha_to_coverage_enabled: false,
-        },
-    );
-
-    let mut swap_chain_descriptor = wgpu::SwapChainDescriptor {
-        usage:        wgpu::TextureUsage::OUTPUT_ATTACHMENT,
-        format:       wgpu::TextureFormat::Bgra8UnormSrgb,
-        width:        size.width,
-        height:       size.height,
-        present_mode: wgpu::PresentMode::Vsync,
-    };
-
-    let mut swap_chain = device.create_swap_chain(
-        &surface,
-        &swap_chain_descriptor,
-    );
-
+    let size = window.0.inner_size();
     let mut screen_size = Screen(
         Vec2::new(
             size.width  as f32,
@@ -154,18 +59,18 @@ pub fn start(mut game: Game) -> Result<(), Error> {
                 screen_size.0.x = size.width  as f32;
                 screen_size.0.y = size.height as f32;
 
-                swap_chain_descriptor.width  = size.width;
-                swap_chain_descriptor.height = size.height;
+                renderer.swap_chain_descriptor.width  = size.width;
+                renderer.swap_chain_descriptor.height = size.height;
 
-                swap_chain = device.create_swap_chain(
-                    &surface,
-                    &swap_chain_descriptor,
+                renderer.swap_chain = renderer.device.create_swap_chain(
+                    &renderer.surface,
+                    &renderer.swap_chain_descriptor,
                 );
             }
             Event::RedrawRequested(_) => {
-                let frame = swap_chain.get_next_texture();
+                let frame = renderer.swap_chain.get_next_texture();
 
-                let mut encoder = device.create_command_encoder(
+                let mut encoder = renderer.device.create_command_encoder(
                     &wgpu::CommandEncoderDescriptor { todo: 0 }
                 );
 
@@ -184,12 +89,12 @@ pub fn start(mut game: Game) -> Result<(), Error> {
                             depth_stencil_attachment: None,
                         },
                     );
-                    render_pass.set_pipeline(&render_pipeline);
-                    render_pass.set_bind_group(0, &bind_group, &[]);
+                    render_pass.set_pipeline(&renderer.render_pipeline);
+                    render_pass.set_bind_group(0, &renderer.bind_group, &[]);
                     render_pass.draw(0 .. 0, 0 .. 0);
                 }
 
-                queue.submit(&[encoder.finish()]);
+                renderer.queue.submit(&[encoder.finish()]);
             }
             Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
                 *control_flow = ControlFlow::Exit
@@ -228,13 +133,6 @@ pub fn start(mut game: Game) -> Result<(), Error> {
 
 #[derive(Debug)]
 pub enum Error {
-    AdapterRequest,
-    Io(io::Error),
+    Renderer(renderer::Error),
     Winit(winit::error::OsError),
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Self {
-        Self::Io(err)
-    }
 }
---
+++ vndf-client/src/frontends/bespoke/renderer.rs
@@ -0,0 +1,145 @@
+use std::io::{
+    self,
+    Cursor,
+};
+
+use super::window::Window;
+
+
+pub struct Renderer {
+    pub surface:               wgpu::Surface,
+    pub device:                wgpu::Device,
+    pub queue:                 wgpu::Queue,
+    pub bind_group:            wgpu::BindGroup,
+    pub render_pipeline:       wgpu::RenderPipeline,
+    pub swap_chain_descriptor: wgpu::SwapChainDescriptor,
+    pub swap_chain:            wgpu::SwapChain,
+}
+
+impl Renderer {
+    pub fn new(window: &Window) -> Result<Self, Error> {
+        let surface = wgpu::Surface::create(&window.0);
+
+        let adapter =
+            wgpu::Adapter::request(
+                &wgpu::RequestAdapterOptions {
+                    power_preference: wgpu::PowerPreference::Default,
+                    backends:         wgpu::BackendBit::all(),
+                },
+            )
+            .ok_or(Error::AdapterRequest)?;
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                extensions: wgpu::Extensions { anisotropic_filtering: false },
+                limits:     wgpu::Limits::default(),
+            },
+        );
+
+        let vertex_shader = include_bytes!("shaders/shader.vert.spv");
+        let vertex_module_module = device.create_shader_module(
+            &wgpu::read_spirv(Cursor::new(&vertex_shader[..]))?,
+        );
+
+        let fragment_shader = include_bytes!("shaders/shader.frag.spv");
+        let fragment_module_module = device.create_shader_module(
+            &wgpu::read_spirv(Cursor::new(&fragment_shader[..]))?,
+        );
+
+        let bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor { bindings: &[] },
+        );
+        let bind_group = device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout:   &bind_group_layout,
+                bindings: &[],
+            },
+        );
+        let pipeline_layout = device.create_pipeline_layout(
+            &wgpu::PipelineLayoutDescriptor {
+                bind_group_layouts: &[&bind_group_layout],
+            },
+        );
+
+        let render_pipeline = device.create_render_pipeline(
+            &wgpu::RenderPipelineDescriptor {
+                layout: &pipeline_layout,
+                vertex_stage: wgpu::ProgrammableStageDescriptor {
+                    module:      &vertex_module_module,
+                    entry_point: "main",
+                },
+                fragment_stage: Some(
+                    wgpu::ProgrammableStageDescriptor {
+                        module:      &fragment_module_module,
+                        entry_point: "main",
+                    }
+                ),
+                rasterization_state: Some(
+                    wgpu::RasterizationStateDescriptor {
+                        front_face:             wgpu::FrontFace::Ccw,
+                        cull_mode:              wgpu::CullMode::None,
+                        depth_bias:             0,
+                        depth_bias_slope_scale: 0.0,
+                        depth_bias_clamp:       0.0,
+                    }
+                ),
+                primitive_topology: wgpu::PrimitiveTopology::TriangleStrip,
+                color_states: &[
+                    wgpu::ColorStateDescriptor {
+                        format:      wgpu::TextureFormat::Bgra8UnormSrgb,
+                        color_blend: wgpu::BlendDescriptor::REPLACE,
+                        alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                        write_mask:  wgpu::ColorWrite::ALL,
+                    },
+                ],
+
+                depth_stencil_state:       None,
+                index_format:              wgpu::IndexFormat::Uint16,
+                vertex_buffers:            &[],
+                sample_count:              1,
+                sample_mask:               !0,
+                alpha_to_coverage_enabled: false,
+            },
+        );
+
+        let size = window.0.inner_size();
+
+        let swap_chain_descriptor = wgpu::SwapChainDescriptor {
+            usage:        wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format:       wgpu::TextureFormat::Bgra8UnormSrgb,
+            width:        size.width,
+            height:       size.height,
+            present_mode: wgpu::PresentMode::Vsync,
+        };
+
+        let swap_chain = device.create_swap_chain(
+            &surface,
+            &swap_chain_descriptor,
+        );
+
+        Ok(
+            Self {
+                surface,
+                device,
+                queue,
+                render_pipeline,
+                bind_group,
+                swap_chain_descriptor,
+                swap_chain,
+            }
+        )
+    }
+}
+
+
+#[derive(Debug)]
+pub enum Error {
+    AdapterRequest,
+    Io(io::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
+    }
+}
