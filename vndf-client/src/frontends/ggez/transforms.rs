6c74b641674f00013f80b141d5328c2769a8dfea
Hanno Braun <hb@hannobraun.de>
2020-04-12 14:08:43 +0200

    Refactor

--- vndf-client/src/frontends/ggez/transforms.rs
+++ vndf-client/src/frontends/ggez/transforms.rs
@@ -2,7 +2,6 @@ use ggez::{
     self,
     Context,
     GameResult,
-    graphics::Rect,
 };
 
 use crate::{
@@ -51,18 +50,13 @@ impl Transform for WorldTransform<'_> {
             ggez::graphics::drawable_size(context);
         let screen_size = graphics::Size::new(screen_width, screen_height);
 
-        let size       = self.camera.world_size_on_screen(screen_size);
-        let upper_left = self.camera.center - size / 2.0;
+        let transform = transforms::world_to_screen(self.camera, screen_size)
+            .post_transform(&transforms::screen_to_homogeneous(screen_size))
+            .to_3d()
+            .to_row_arrays();
 
-        ggez::graphics::set_screen_coordinates(
-            context,
-            Rect {
-                x: upper_left.x,
-                y: upper_left.y,
-                w: size.width,
-                h: size.height,
-            },
-        )?;
+        ggez::graphics::set_projection(context, transform);
+        ggez::graphics::apply_transformations(context)?;
 
         Ok(())
     }
