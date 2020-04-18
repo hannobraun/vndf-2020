3b23ab457f78bd066a63d34d9ade18e1b03e925e
Hanno Braun <hb@hannobraun.de>
2020-04-18 13:02:57 +0200

    Define type for shader results

--- vndf-client/src/frontends/bespoke/shaders.rs
+++ vndf-client/src/frontends/bespoke/shaders.rs
@@ -4,12 +4,13 @@ use std::io::{
 };
 
 
-pub fn vertex_shader(device: &wgpu::Device)
-    -> Result<wgpu::ShaderModule, io::Error>
-{
+pub fn vertex_shader(device: &wgpu::Device) -> Result {
     let code = include_bytes!("shaders/shader.vert.spv");
     let module = device.create_shader_module(
         &wgpu::read_spirv(Cursor::new(&code[..]))?,
     );
     Ok(module)
 }
+
+
+pub type Result = std::result::Result<wgpu::ShaderModule, io::Error>;
