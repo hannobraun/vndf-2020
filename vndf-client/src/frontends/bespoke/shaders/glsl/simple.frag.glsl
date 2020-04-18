6bc7ce4d92bf2042250207c25adf874909f485b2
Hanno Braun <hb@hannobraun.de>
2020-04-18 15:13:10 +0200

    Pass vertex position to fragment shader

--- vndf-client/src/frontends/bespoke/shaders/glsl/simple.frag.glsl
+++ vndf-client/src/frontends/bespoke/shaders/glsl/simple.frag.glsl
@@ -5,6 +5,7 @@
 
 #version 450
 
+layout(location = 0) in  vec2 pos;
 layout(location = 0) out vec4 color;
 
 void main() {
--- vndf-client/src/frontends/bespoke/shaders/glsl/simple.vert.glsl
+++ vndf-client/src/frontends/bespoke/shaders/glsl/simple.vert.glsl
@@ -10,9 +10,11 @@ layout(set = 0, binding = 0) uniform Locals {
     mat4 transform;
 };
 
-layout(location = 0) in vec2 pos_in;
+layout(location = 0) in  vec2 pos_in;
+layout(location = 0) out vec2 pos_out;
 
 
 void main() {
+    pos_out = pos_in;
     gl_Position = transform * vec4(pos_in, 0.0, 1.0);
 }
