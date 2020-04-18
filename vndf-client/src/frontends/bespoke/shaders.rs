4c2749798320771b8780986b9bb49967ec3170f2
Hanno Braun <hb@hannobraun.de>
2020-04-18 13:41:06 +0200

    Refactor

--- vndf-client/src/frontends/bespoke/shaders.rs
+++ vndf-client/src/frontends/bespoke/shaders.rs
@@ -5,26 +5,35 @@ use std::io::{
 
 
 pub fn vertex_shader(device: &wgpu::Device) -> Result {
-    Shader::Vertex.load(device)
+    Shader::Vertex(VertexShader::Simple).load(device)
 }
 
 pub fn fragment_shader(device: &wgpu::Device) -> Result {
-    Shader::Fragment.load(device)
+    Shader::Fragment(FragmentShader::Simple).load(device)
+}
+
+
+pub enum VertexShader {
+    Simple,
+}
+
+pub enum FragmentShader {
+    Simple,
 }
 
 
 enum Shader {
-    Vertex,
-    Fragment,
+    Vertex(VertexShader),
+    Fragment(FragmentShader),
 }
 
 impl Shader {
     fn load(&self, device: &wgpu::Device) -> Result {
         let code = match self {
-            Shader::Vertex => {
+            Shader::Vertex(VertexShader::Simple) => {
                 &include_bytes!("shaders/spv/simple.vert.spv")[..]
             }
-            Shader::Fragment => {
+            Shader::Fragment(FragmentShader::Simple) => {
                 &include_bytes!("shaders/spv/simple.frag.spv")[..]
             }
         };
