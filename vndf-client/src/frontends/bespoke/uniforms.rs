f957115595c6216659021224487c160d6466d642
Hanno Braun <hanno@braun-embedded.com>
2020-04-23 13:49:46 +0200

    Refactor

--- vndf-client/src/frontends/bespoke/uniforms.rs
+++ vndf-client/src/frontends/bespoke/uniforms.rs
@@ -27,7 +27,7 @@ impl Default for Uniforms {
 
         Self {
             transform,
-            color:       Color([1.0; 4]),
+            color:       Color::default(),
             u_per_pixel: [1.0, 1.0],
         }
     }
@@ -37,6 +37,12 @@ impl Default for Uniforms {
 #[derive(AsBytes)]
 #[repr(packed)]
 pub struct Color(pub [f32; 4]);
+
+impl Default for Color {
+    fn default() -> Self {
+        Color([1.0; 4])
+    }
+}
 
 impl From<[f32; 3]> for Color {
     fn from([r, g, b]: [f32; 3]) -> Self {
