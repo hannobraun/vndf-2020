037351896e5a3516bbdd72a41c58afb80cc3d38f
Hanno Braun <hb@hannobraun.de>
2020-04-27 23:30:33 +0200

    Remove unused code

--- vndf-client/src/frontends/ggez/draw.rs
+++ vndf-client/src/frontends/ggez/draw.rs
@@ -4,18 +4,6 @@ use ggez::{
     graphics::Drawable,
 };
 
-use crate::{
-    game::camera::Camera,
-    graphics::{
-        self,
-        elements::{
-            UiElement,
-            WorldElement,
-        },
-    },
-    shared::world,
-};
-
 
 pub fn draw<D>(
     context:   &mut Context,
@@ -40,61 +28,4 @@ pub fn draw<D>(
         drawable,
         param,
     )
-}
-
-
-pub trait Transform {
-    type Point;
-    type Vector;
-
-    fn enable(&self, _: &mut Context) -> GameResult;
-}
-
-
-pub struct ScreenTransform<'r> {
-    pub element: &'r UiElement,
-}
-
-impl Transform for ScreenTransform<'_> {
-    type Point  = graphics::Pnt2;
-    type Vector = graphics::Vec2;
-
-    fn enable(&self, context: &mut Context) -> GameResult {
-        let (width, height) = ggez::graphics::drawable_size(context);
-        let screen_size = graphics::Size::new(width, height);
-
-        let transform = self.element
-            .transform(screen_size)
-            .to_native();
-
-        ggez::graphics::set_projection(context, transform);
-        ggez::graphics::apply_transformations(context)?;
-
-        Ok(())
-    }
-}
-
-
-pub struct WorldTransform<'r> {
-    pub element: &'r WorldElement,
-    pub camera:  &'r Camera,
-}
-
-impl Transform for WorldTransform<'_> {
-    type Point  = world::Pnt2;
-    type Vector = world::Vec2;
-
-    fn enable(&self, context: &mut Context) -> GameResult {
-        let (width, height) = ggez::graphics::drawable_size(context);
-        let screen_size = graphics::Size::new(width, height);
-
-        let transform = self.element
-            .transform(self.camera, screen_size)
-            .to_native();
-
-        ggez::graphics::set_projection(context, transform);
-        ggez::graphics::apply_transformations(context)?;
-
-        Ok(())
-    }
 }
