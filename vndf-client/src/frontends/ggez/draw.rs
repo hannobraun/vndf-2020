d84be733b11b0d864bb0669d5b5976df7e95b7f9
Hanno Braun <hb@hannobraun.de>
2020-04-27 22:22:55 +0200

    Remove unused code

--- vndf-client/src/frontends/ggez/draw.rs
+++ vndf-client/src/frontends/ggez/draw.rs
@@ -76,10 +76,6 @@ impl<P, V> DrawParam<P, V>
         Self(self.0.scale(scale.into()), PhantomData, PhantomData)
     }
 
-    pub fn rotation(self, rotation: f32) -> Self {
-        Self(self.0.rotation(rotation), PhantomData, PhantomData)
-    }
-
     pub fn color(self, color: impl Into<Color>) -> Self {
         Self(self.0.color(color.into()), PhantomData, PhantomData)
     }
