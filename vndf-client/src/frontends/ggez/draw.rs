4e4dc4f43525ba11264f55e69f823c1804192c05
Hanno Braun <hb@hannobraun.de>
2020-04-27 23:08:41 +0200

    Remove unused code

--- vndf-client/src/frontends/ggez/draw.rs
+++ vndf-client/src/frontends/ggez/draw.rs
@@ -1,18 +1,7 @@
-use core::marker::PhantomData;
-
 use ggez::{
     Context,
     GameResult,
-    graphics::{
-        Color,
-        Drawable,
-    },
-    mint,
-};
-
-use crate::{
-    graphics,
-    shared::world,
+    graphics::Drawable,
 };
 
 
@@ -47,39 +36,4 @@ pub trait Transform {
     type Vector;
 
     fn enable(&self, _: &mut Context) -> GameResult;
-}
-
-
-pub struct DrawParam<P, V>(
-    ggez::graphics::DrawParam,
-    PhantomData<P>,
-    PhantomData<V>,
-);
-
-impl DrawParam<graphics::Pnt2, graphics::Vec2> {
-    pub fn screen() -> Self {
-        Self(ggez::graphics::DrawParam::new(), PhantomData, PhantomData)
-    }
-}
-
-impl DrawParam<world::Pnt2, world::Vec2> {
-    pub fn world() -> Self {
-        Self(ggez::graphics::DrawParam::new(), PhantomData, PhantomData)
-    }
-}
-
-impl<P, V> DrawParam<P, V>
-    where
-        P: Into<mint::Point2<f32>>,
-        V: Into<mint::Vector2<f32>>,
-{
-    pub fn color(self, color: impl Into<Color>) -> Self {
-        Self(self.0.color(color.into()), PhantomData, PhantomData)
-    }
-}
-
-impl<P, V> From<DrawParam<P, V>> for ggez::graphics::DrawParam {
-    fn from(from: DrawParam<P, V>) -> Self {
-        from.0
-    }
 }
