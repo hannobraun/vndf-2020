579d2cce2fe046cf5727652d79a8869721b80830
Hanno Braun <hb@hannobraun.de>
2020-05-13 21:15:15 +0200

    Move fonts directory

--- vndf-client/src/frontend/ui/basic.rs
+++ vndf-client/src/frontend/ui/basic.rs
@@ -20,7 +20,7 @@ impl Ui {
     pub fn new(device: &wgpu::Device, texture_format: wgpu::TextureFormat)
         -> Result<Self, wgpu_glyph::rusttype::Error>
     {
-        let font = include_bytes!("../fonts/Tuffy_Bold.ttf");
+        let font = include_bytes!("fonts/Tuffy_Bold.ttf");
         let glyph_brush = GlyphBrushBuilder::using_font_bytes(&font[..])?
             .build(&device, texture_format);
 
