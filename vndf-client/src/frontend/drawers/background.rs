db67abd2a62057ad75a3ed362011bb21d86d223b
Hanno Braun <hb@hannobraun.de>
2020-05-24 09:13:51 +0200

    Rename variable

--- vndf-client/src/frontend/drawers/background.rs
+++ vndf-client/src/frontend/drawers/background.rs
@@ -3,12 +3,12 @@ use crate::graphics;
 use super::Frame;
 
 
-pub fn draw_background(res: &mut Frame) {
-    res.encoder.begin_render_pass(
+pub fn draw_background(frame: &mut Frame) {
+    frame.encoder.begin_render_pass(
         &wgpu::RenderPassDescriptor {
             color_attachments: &[
                 wgpu::RenderPassColorAttachmentDescriptor {
-                    attachment:     &res.output.view,
+                    attachment:     &frame.output.view,
                     resolve_target: None,
                     load_op:        wgpu::LoadOp::Clear,
                     store_op:       wgpu::StoreOp::Store,
