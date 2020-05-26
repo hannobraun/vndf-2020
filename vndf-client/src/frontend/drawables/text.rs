f9af92750028bb0c4a7064a1764f6678cfb82b13
Hanno Braun <hb@hannobraun.de>
2020-05-26 13:15:59 +0200

    Refactor

--- vndf-client/src/frontend/drawables/text.rs
+++ vndf-client/src/frontend/drawables/text.rs
@@ -5,7 +5,10 @@ use wgpu_glyph::{
     Section,
 };
 
-use crate::graphics;
+use crate::{
+    frontend::drawers::Frame,
+    graphics,
+};
 
 
 pub struct Text {
@@ -32,5 +35,27 @@ impl Text {
             .map(|size| {
                 graphics::Size::new(size.width(), size.height())
             })
+    }
+
+    pub fn draw<'r>(&mut self,
+        device:   &wgpu::Device,
+        frame:    &mut Frame,
+        sections: impl IntoIterator<Item=Section<'r>>,
+    ) {
+        for section in sections {
+            self.glyph_brush.queue(section);
+        }
+
+        self.glyph_brush
+            .draw_queued(
+                &device,
+                &mut frame.encoder,
+                &frame.output.view,
+                frame.screen.size.width as u32,
+                frame.screen.size.height as u32,
+            )
+            // I've checked the code, and it doesn't look like this
+            // actually returns any errors.
+            .unwrap();
     }
 }
--- vndf-client/src/frontend/ui/basic.rs
+++ vndf-client/src/frontend/ui/basic.rs
@@ -43,58 +43,57 @@ impl super::Ui for Basic {
     {
         let scale  = Scale::uniform(16.0 * frame.screen.scale_factor);
 
-        for element in ui::Elements::new(game, &frame.screen).elements() {
-            let text  = element.text.as_str();
-            let pos   = element.pos * frame.screen.scale_factor;
-            let color = [1.0, 1.0, 1.0, 1.0];
+        let elements = ui::Elements::new(game, &frame.screen);
+        let sections: Vec<_> = elements
+            .elements()
+            .into_iter()
+            .filter_map(|element| {
+                let text  = element.text.as_str();
+                let pos   = element.pos * frame.screen.scale_factor;
+                let color = [1.0, 1.0, 1.0, 1.0];
 
-            let section = Section {
-                text,
-                screen_position: (pos.x, pos.y),
-                scale,
-                color,
-                .. Section::default()
-            };
+                let section = Section {
+                    text,
+                    screen_position: (pos.x, pos.y),
+                    scale,
+                    color,
+                    .. Section::default()
+                };
 
-            let size = match res.drawables.text.bounds(section) {
-                Some(size) => size / frame.screen.scale_factor,
-                None       => continue,
-            };
+                let size = match res.drawables.text.bounds(section) {
+                    Some(size) => size / frame.screen.scale_factor,
+                    None       => return None,
+                };
 
-            const MARGIN: f32 = 5.0;
-            let margin = graphics::Size::new(MARGIN * 2.0, MARGIN * 2.0);
+                const MARGIN: f32 = 5.0;
+                let margin = graphics::Size::new(MARGIN * 2.0, MARGIN * 2.0);
 
-            let element = ScreenElement {
-                size:  size + margin,
-                pos:   pos + size * frame.screen.scale_factor / 2.0,
-                angle: graphics::Angle::zero(),
-            };
+                let element = ScreenElement {
+                    size:  size + margin,
+                    pos:   pos + size * frame.screen.scale_factor / 2.0,
+                    angle: graphics::Angle::zero(),
+                };
 
-            res.drawables.panel.draw(
-                &res.device,
-                frame,
-                vert::simple::Uniforms {
-                    transform: element.transform(frame.screen.size).into(),
-                },
-                frag::simple::Uniforms {
-                    color: [0.0, 0.0, 0.0, 0.95].into(),
-                },
-            );
+                res.drawables.panel.draw(
+                    &res.device,
+                    frame,
+                    vert::simple::Uniforms {
+                        transform: element.transform(frame.screen.size).into(),
+                    },
+                    frag::simple::Uniforms {
+                        color: [0.0, 0.0, 0.0, 0.95].into(),
+                    },
+                );
 
-            res.drawables.text.glyph_brush.queue(section);
-        }
+                Some(section)
+            })
+            .collect();
 
-        res.drawables.text.glyph_brush
-            .draw_queued(
-                &res.device,
-                &mut frame.encoder,
-                &frame.output.view,
-                frame.screen.size.width as u32,
-                frame.screen.size.height as u32,
-            )
-            // I've checked the code, and it doesn't look like this
-            // actually returns any errors.
-            .unwrap();
+        res.drawables.text.draw(
+            &res.device,
+            frame,
+            sections,
+        );
 
         Ok(())
     }
