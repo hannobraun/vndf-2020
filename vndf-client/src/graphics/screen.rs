06e27118ad2837b828d1bb6201b5f867e2d5d78f
Hanno Braun <hb@hannobraun.de>
2020-06-07 00:45:57 +0200

    Make field private

--- vndf-client/src/graphics/screen.rs
+++ vndf-client/src/graphics/screen.rs
@@ -3,8 +3,7 @@ use crate::graphics;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Screen {
-    /// The physical screen size in pixels
-    pub size: graphics::Size,
+    size: graphics::Size,
 
     /// The scale factor used to convert between physical and logical size
     pub scale_factor: graphics::Scalar,
