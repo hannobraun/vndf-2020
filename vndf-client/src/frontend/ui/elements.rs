bdd5e09d9cdef0d1953cc230adde6acece28da20
Hanno Braun <hb@hannobraun.de>
2020-06-12 13:42:40 +0200

    Rename module

--- vndf-client/src/frontend/ui/elements.rs
+++ vndf-client/src/frontend/ui/elements.rs
@@ -1,4 +1,4 @@
-mod diagnostics;
+mod component_stats;
 mod frame_time;
 mod input_events;
 mod instructions;
@@ -10,7 +10,7 @@ mod text_panel;
 
 
 pub use self::{
-    diagnostics::ComponentStats,
+    component_stats::ComponentStats,
     frame_time::FrameTime,
     input_events::InputEvents,
     instructions::Instructions,
