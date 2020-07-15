ad5e40ee70777cbf7f802cc219c3f4caf465c104
Hanno Braun <hb@hannobraun.de>
2020-07-15 15:47:27 +0200

    Rename variable

--- vndf-macros/src/ui.rs
+++ vndf-macros/src/ui.rs
@@ -83,12 +83,12 @@ pub fn derive(
             )
         });
 
-    let struct_  = struct_.ident;
+    let name     = struct_.ident;
     let arg_name = arg_name.into_iter();
     let arg_ty   = arg_ty.into_iter();
 
     let tokens = quote!(
-        impl #trait_ for #struct_ {
+        impl #trait_ for #name {
             fn #method(&mut self,
                 #(
                     #arg_name: #arg_ty,
